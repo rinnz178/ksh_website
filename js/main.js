6695bed21838e4f165560d3cf407068adedfe914
rinnz178 <95839418+rinnz178@users.noreply.github.com>
2024-04-26 15:27:32 +0630

    feat : add activities section and change spacing

--- js/main.js
+++ js/main.js
@@ -28,5 +28,33 @@ function toggleService(id) {
 });
 
 
+// Scroll Transition
+
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to handle scroll event
+function handleScroll() {
+  const sections = document.querySelectorAll('section');
+  sections.forEach(section => {
+    if (isInViewport(section)) {
+      section.classList.add('show');
+      window.removeEventListener('scroll', handleScroll); // Remove scroll event listener once animation is triggered
+    }
+  });
+}
+
+// Add scroll event listener
+window.addEventListener('scroll', handleScroll);
+
+
 
   
